c24ca833a6e98db748541147614416de3998d334
Leejin-Yang <ejin1996@gmail.com>
2021-08-26 13:40:37 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+//  application/x-www-form-urlencoded
+//  app.use(bodyParser.urlencoded({ extended: true }));
+//  application/json
+//  app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json());
 
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://leejinyang:qwerasdf@boilerplate.v7hqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = config.mongoURI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,6 +25,16 @@ client.connect((err) => {
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //  회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //  그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
