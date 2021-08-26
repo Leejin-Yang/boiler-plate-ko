f144ecd69f1f9a0b5fcf0122f947c37759910170
Leejin-Yang <ejin1996@gmail.com>
2021-08-26 16:47:05 +0900

    Auth 기능

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const auth = require("./middleware/auth");
 const port = 5000;
 const config = require("./config/key");
 const { User } = require("./models/User");
@@ -26,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   //  회원 가입 할때 필요한 정보들을 client에서 가져오면
   //  그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
@@ -36,7 +37,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   //  요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -67,6 +68,21 @@ app.post("/login", (req, res) => {
         .status(200)
         .json({ loginSuccess: true, userID: user._id });
     });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //  여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True
+  //  role === 0 일반유저, role !== 0 관리자
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
