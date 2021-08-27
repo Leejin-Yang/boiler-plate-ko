9b707aa4d1d73e07185c9b63f529e17db0ac84d2
Leejin-Yang <ejin1996@gmail.com>
2021-08-27 16:20:46 +0900

    로그인 기능 구현

---
+++ client/src/_actions/types.js
@@ -0,0 +1,3 @@
+//  모든 타입 지정
+
+export const LOGIN_USER = "login_user";
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,63 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+function LoginPage(props) {
+  const dispatch = useDispatch();
+
+  //props state
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  //  타이핑을 할 때 onChange라는 이벤트를 발생시켜 State를 변화 -> value 변화
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    //  안해주면 페이지 리프레시
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
